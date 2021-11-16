80d073118882f3f37490f1eaa34203bc85f1142d
deftthunk <35551306+deftthunk@users.noreply.github.com>
2021-11-15 23:40:38 -0500

    fixed progress tracking bug

--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/deftthunk/argonbreak
 go 1.17
 
 require (
-	github.com/golang-collections/go-datastructures v0.0.0-20150211160725-59788d5eb259
+	github.com/Workiva/go-datastructures v1.0.53
 	github.com/matthewhartstonge/argon2 v0.1.5
 )
 
 require (
-	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- main.go
+++ main.go
@@ -1,113 +1,111 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "bufio"
-    "log"
-    "os"
-    "github.com/matthewhartstonge/argon2"
-    ds "github.com/golang-collections/go-datastructures/queue"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Workiva/go-datastructures/queue"
+	"github.com/matthewhartstonge/argon2"
 )
 
 var pf = fmt.Printf
+var pl = fmt.Println
 
 func argon_config() argon2.Config {
-    argon := argon2.DefaultConfig()
+	argon := argon2.DefaultConfig()
 
-    argon.HashLength = 32
-    argon.SaltLength = 16
-    argon.TimeCost = 2
-    argon.MemoryCost = 10 * 1024
-    argon.Parallelism = 2
-    argon.Mode = argon2.ModeArgon2id
-    argon.Version = argon2.Version13
+	argon.HashLength = 64
+	argon.SaltLength = 16
+	argon.TimeCost = 5
+	argon.MemoryCost = 10 * 1024
+	argon.Parallelism = 2
+	argon.Mode = argon2.ModeArgon2id
+	argon.Version = argon2.Version13
 
-    return argon
+	return argon
 }
 
 func main() {
-    var threads int = 4
-    var ch chan []byte = make(chan []byte, threads)
-    var counter int = 0
-    var block_size int = 100
-    var word string = ""
-    var win string = "FOUNDIT!"
+	var threads int = 8
+	var ch chan []byte = make(chan []byte, threads)
+	var counter int = 0
+	var block_size int = 50
+	var word string = ""
+	var win string = "FOUNDIT!"
 
-    file, err := os.Open("passwords_10m.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-    scanner := bufio.NewScanner(file)
-    argon := argon_config()
+	file, err := os.Open("passwords_10m.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	argon := argon_config()
 
-    status := func() {
-        rb := ds.NewRingBuffer(10)
-        begin := time.Now()
-        delay := 1 * time.Second
-        last, rate := 0, 0
+	status := func() {
+		buf_size := int64(30)
+		hist_buffer := queue.New(buf_size)
+		delay := 1 * time.Second
+		rate, average := float64(0), float64(0)
 
-        for {
-            rb.Put(counter)
-            time.Sleep(delay)
+		for {
+			hist_buffer.Put(counter)
+			time.Sleep(delay)
 
-            pf("\r                                        ")
-            pf("\rCur: %s\tRate: %d/sec\tTotal: %d        ", word, rate, counter)
+			pf("\r                                            ")
+			pf("\rCur: %s\t  Rate: %.2f/sec\tTotal: %d        ", word, rate, counter)
 
-            // FIX averaging not working yet
-            if time.Since(begin) > (3 * time.Second) {
-                begin = time.Now()
-                rate = (counter - last) / 3
-                placeholder, _ := rb.Get()
-                last = placeholder.(int)
-            }
+			if hist_buffer.Len() >= buf_size {
+				buf_item, _ := hist_buffer.Get(1)
+				popped_count := float64(buf_item[0].(int))
+				buf_item2, _ := hist_buffer.Peek()
+				next_count := float64(buf_item2.(int))
 
-            if rb.Len() > 9 {
-                _, _ = rb.Get()
-            }
-        }
-    }
-    go status()
+				average = (average + popped_count + next_count) / 3
+				rate = (float64(counter) - average) / float64(buf_size)
+			} else {
+				last, _ := hist_buffer.Peek()
+				average = (average + float64(last.(int))) / 2
+				rate = (float64(counter) - average) / float64(hist_buffer.Len())
+			}
+		}
+	}
+	go status()
 
-    // spinup
-    for i:=0; i<threads; i++ {
-        go crack(argon, []string{"spinup"}, win, ch)
-    }
+	thread_count := 0
+	for {
+		work_block := make([]string, block_size)
+		for i := 0; i < block_size; i++ {
+			scanner.Scan()
+			work_block[i] = scanner.Text()
+		}
 
-    for {
-        work_block := make([]string, block_size)
-        for i:=0; i<block_size; i++ {
-            scanner.Scan()
-            work_block[i] = scanner.Text()
-        }
+		word = work_block[0]
+		// spinup set number of threads
+		if thread_count < threads {
+			go crack(argon, work_block, win, ch)
+			counter += block_size
+			thread_count++
+			time.Sleep(800 * time.Millisecond)
+			continue
+		}
 
-        word = work_block[0]
-
-        select {
-            case retn := <-ch:
-                if string(retn) == win {
-                    fmt.Println("Successfully cracked password")
-                }
-                go crack(argon, work_block, win, ch)
-                counter += block_size
-                continue
-        }
-    }
+		retn := <-ch
+		if string(retn) == win {
+			pl("Successfully cracked password")
+		}
+		go crack(argon, work_block, win, ch)
+		counter += block_size
+	}
 }
 
 func crack(argon argon2.Config, work_block []string, win string, ch chan []byte) {
-    var hash []byte
-    for _, password := range work_block {
-        hash, _ = argon.HashEncoded([]byte(password))
-    }
+	var hash []byte
+	for _, password := range work_block {
+		hash, _ = argon.HashEncoded([]byte(password))
+	}
 
-    // PLACEHOLDER
-    if win == string(hash) {
-        fmt.Println("yay we found it")
-    }
-    ch<-hash
+	ch <- hash
 }
-
-
-
